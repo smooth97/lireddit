2c2405f1f45caacc893ac10c5d679af4ef9119a6
seungjin <bak0506@ddive.co>
2021-06-04 12:39:32 +0900

    setting: node & ts & graphql & mikro orm

---
+++ lireddit-server/src/index.ts
@@ -0,0 +1,16 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: "first post" });
+  await orm.em.persistAndFlush(post);
+  await orm.em.nativeInsert(Post, { title: "second post" });
+};
+
+main().catch((err) => {
+  console.log("err", err);
+});
