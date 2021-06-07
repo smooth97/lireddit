bcec72013847384dc94d874896960f7ea804a021
seungjin <bak0506@ddive.co>
2021-06-07 17:23:14 +0900

    apollo server express setup

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,14 +1,37 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "first post" });
-  await orm.em.persistAndFlush(post);
-  await orm.em.nativeInsert(Post, { title: "second post" });
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+  app.listen(4000, () => {
+    console.log("server started on 4000");
+  });
 };
 
 main().catch((err) => {
