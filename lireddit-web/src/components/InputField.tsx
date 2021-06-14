07f9ecbe88685a03899ecdda137324551a9e39a7
smooth97 <dbtmdwls0506@naver.com>
2021-06-14 22:15:26 +0900

    feat: form

--- lireddit-web/src/components/InputField.tsx
+++ lireddit-web/src/components/InputField.tsx
@@ -13,7 +13,11 @@ type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   placeholder?: string;
 };
 
-const InputField: React.FC<InputFieldProps> = (props) => {
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
   const [field, { error }] = useField(props);
 
   /** isInvalid={!!error}
@@ -23,8 +27,13 @@ const InputField: React.FC<InputFieldProps> = (props) => {
 
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
       <FormErrorMessage>{error}</FormErrorMessage>
     </FormControl>
   );
