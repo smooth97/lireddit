03c9df7f3421c1ac3ce7c9338ec974a7210ab19f
ddive-sjin <dbtmdwls0506@naver.com>
2021-08-02 21:14:53 +0900

    feat: next.js query params

--- lireddit-web/src/pages/change-password/[token].tsx
+++ lireddit-web/src/pages/change-password/[token].tsx
@@ -11,7 +11,7 @@ import { withUrqlClient, WithUrqlProps } from "next-urql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 import NextLink from "next/link";
 
-const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+const ChangePassword: NextPage = () => {
   const router = useRouter();
   const [, changePassword] = useChangePasswordMutation();
   const [tokenError, setTokenError] = useState("");
@@ -23,7 +23,8 @@ const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
         onSubmit={async (values, { setErrors }) => {
           const response = await changePassword({
             newPassword: values.newPassword,
-            token,
+            token:
+              typeof router.query.token === "string" ? router.query.token : "",
           });
           if (response.data?.changePassword.errors) {
             const errorMap = toErrorMap(response.data.changePassword.errors);
@@ -70,13 +71,12 @@ const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
   );
 };
 
-ChangePassword.getInitialProps = ({ query }) => {
-  return {
-    token: query.token as string,
-  };
-};
-
-//export default withUrqlClient(createUrqlClient)(ChangePassword as any);
+// 06:30
+// ChangePassword.getInitialProps = ({ query }) => {
+//   return {
+//     token: query.token as string,
+//   };
+// };
 
 export default withUrqlClient(createUrqlClient)(
   ChangePassword as FC<PropsWithChildren<WithUrqlProps | { token: string }>>
